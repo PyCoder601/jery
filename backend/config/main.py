efc3c059d541b0a035eb34d447d29bbd598ccba1
romeomanoela <romeomanoela18@gmail.com>
2025-10-02 20:58:00 +0300

    ♻️ refactor(config): Permet plusieurs origines CORS via une variable d'environnement

--- backend/config/main.py
+++ backend/config/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("ALLOWED_ORIGins"), "http://localhost:3000"],
+    allow_origins=[link for link in os.getenv("ALLOWED_ORIGINS").split(",")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
