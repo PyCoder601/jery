b6a81b27ebea9ba079cfe8007bde3453a10cce94
romeomanoela <romeomanoela18@gmail.com>
2026-02-19 20:56:03 +0300

    ✨ refactor(hooks): Remove unused useStore hook and update TypeScript configuration for improved JSX support

--- frontend/src/redux/hooks.ts
+++ frontend/src/redux/hooks.ts
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector, useStore } from "react-redux";
-import type { AppDispatch, RootState, AppStore } from "./store";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "./store";
 
 // Hooks typés à utiliser dans toute l'application au lieu des hooks non typés
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
